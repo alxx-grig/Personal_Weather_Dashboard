314dfafc3e22ea0bf6bc8fedfb10749553ea0931
Pablo Garcia <pablodgarl@gmail.com>
2026-03-24 13:30:22 -0400

    Started working on the actual weather widget

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function updateClock(){
   const now = new Date(); // Creates a Date object called 'now' to access the date and time
 
-  const hours = now.getHours().toString().padStart(2, '0');
+  const hours = now.getHours().toString().padStart(2, '0'); // .padStart() function adds whatever character is specified at the beginning of the string if it doesn't have the required number of characters: in this case 2
   const minutes = now.getMinutes().toString().padStart(2, '0');
   const seconds = now.getSeconds().toString().padStart(2, '0');
 
@@ -13,3 +13,13 @@ function updateClock(){
 
 updateClock();
 setInterval(updateClock, 1000);
+
+async function getWeather() { // async is a keyword that is used when we don't really want a function to activate instantly and is waiting for another condition
+  const apiKey = '';
+  const city = 'Orlando';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`; // will be used to access the weather API
+
+
+
+
+}
